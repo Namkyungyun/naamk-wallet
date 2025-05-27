0bd131036aef73dd8180a1f416065177b5600a73
윤남경 <nk.yun@bpmg.biz>
2025-05-27 18:27:22 +0900

    fix : 테스트 코드 내 변경 건 적용

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,12 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:naamk_wallet/ui/root_view.dart';
+import 'package:naamk_wallet/app/root_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const RootView());
+    // await tester.pumpWidget(const RootScreen(default));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
