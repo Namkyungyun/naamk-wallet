3ed17fc85cf9f2787609ac83f480ad70643ce125
윤남경 <nk.yun@bpmg.biz>
2025-06-13 17:47:00 +0900

    feat : 앱 메인 screen의 titleAppbar widget 작업

---
+++ lib/common/widgets/title_appbar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:naamk_wallet/config/di/ui_common_module.dart';
+
+class TitleAppbarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final double toolbarHeight = 60;
+
+  const TitleAppbarWidget({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title,
+        textAlign: TextAlign.start,
+      ),
+      excludeHeaderSemantics: true,
+      backgroundColor: Colors.transparent,
+      shadowColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      scrolledUnderElevation: 0,
+      elevation: 0,
+      bottomOpacity: 0,
+      toolbarHeight: toolbarHeight,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(toolbarHeight);
+}
