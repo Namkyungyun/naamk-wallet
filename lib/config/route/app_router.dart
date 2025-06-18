cc0b68b9bceb7b9880230784fa51b0d6c12f15b9
윤남경 <nk.yun@bpmg.biz>
2025-06-18 11:50:04 +0900

    feat : go_router의 StatefulShellRoute 적용 (추후 딥링크 연동까지 일관되게하기 위해)

--- lib/config/route/app_router.dart
+++ lib/config/route/app_router.dart
@@ -1,30 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:naamk_wallet/app/app_shell.dart';
+import 'package:naamk_wallet/app/event/event_screen.dart';
 import 'package:naamk_wallet/app/event/spining_wheel/spinning_wheel_page.dart';
+import 'package:naamk_wallet/app/home/home_screen.dart';
+import 'package:naamk_wallet/app/notice/notice_screen.dart';
 import 'package:naamk_wallet/app/setting/setting_screen.dart';
+import 'package:naamk_wallet/app/wallet/wallet_screen.dart';
 import 'package:naamk_wallet/config/route/app_route_path.dart';
 import 'package:naamk_wallet/config/route/fade_extension.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+
 class AppRouter {
   final GoRouter goRoute = GoRouter(
-    initialLocation: AppRoute.home.route,
-    routes: <GoRoute>[
+    navigatorKey: _rootNavigatorKey,
+    initialLocation: '/home',
+    debugLogDiagnostics: true,
+    routes: [
+      StatefulShellRoute.indexedStack(
+        builder: (context, state, navigationShell) => AppShell(
+          navigationShell: navigationShell,
+        ),
+        branches: <StatefulShellBranch>[
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoute.notice.route,
+                builder: (context, state) => const NoticeScreen(),
+              ).fade(),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoute.wallet.route,
+                builder: (context, state) => const WalletScreen(),
+              ).fade(),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoute.home.route,
+                builder: (context, state) => const HomeScreen(),
+              ).fade(),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoute.event.route,
+                builder: (context, state) => const EventScreen(),
+              ).fade(),
+            ],
+          ),
+          StatefulShellBranch(
+            routes: [
+              GoRoute(
+                path: AppRoute.settings.route,
+                builder: (context, state) => const SettingScreen(),
+              ).fade(),
+            ],
+          ),
+        ],
+      ),
       GoRoute(
-        path: AppRoute.home.route,
-        builder: (BuildContext context, GoRouterState state) =>
-            const AppShell(),
-      ).fade(),
-      GoRoute(
-        path: AppRoute.settings.route,
-        builder: (BuildContext context, GoRouterState state) =>
-            const SettingScreen(),
-      ).fade(),
-      GoRoute(
-        path: AppRoute.event.route,
-        builder: (BuildContext context, GoRouterState state) =>
-            const SpinningWheelPage(),
-      ).fade(),
+        path: '${AppRoute.event.route}/rullet',
+        parentNavigatorKey: _rootNavigatorKey, // 중요!
+        builder: (context, state) => const SpinningWheelPage(),
+      ).fade()
     ],
   );
   GoRouter get getGoRouter => goRoute;
