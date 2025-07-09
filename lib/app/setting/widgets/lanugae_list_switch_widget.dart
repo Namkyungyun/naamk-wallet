f098c4626db40dab93faaea5e5a15a3806476326
윤남경 <nk.yun@bpmg.biz>
2025-07-09 12:08:51 +0900

    fix : [refactor] 전체 리빌드 안되도록 최적화 진행 :: LanguageListSwitchWigdet

--- lib/app/setting/widgets/lanugae_list_switch_widget.dart
+++ lib/app/setting/widgets/lanugae_list_switch_widget.dart
@@ -1,74 +1,107 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:naamk_wallet/config/presentation/ui_common_module.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:naamk_wallet/app/setting/widgets/setting_info_listtile_widget.dart';
 import 'package:naamk_wallet/config/presentation/language/app_language.dart';
 import 'package:naamk_wallet/config/presentation/language/app_language_state.dart';
 
-class LanguageListSwitchWidget extends HookConsumerWidget {
-  const LanguageListSwitchWidget({super.key});
+class LanguageListSwitchWidget extends StatelessWidget {
+  LanguageListSwitchWidget({super.key});
+
+  // 전체 빌드가 일어나지 않고, ValueListenableBuilder 내부만 빌드가 일어나도록.
+  final ValueNotifier<bool> _showLanguageCard = ValueNotifier(false);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final ValueNotifier<bool> showLanguageCard = useState(false); // 지역 state 관리
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        LanguageSwitchWidget(
+          onTab: () => _showLanguageCard.value = !_showLanguageCard.value,
+        ),
+        ValueListenableBuilder<bool>(
+          valueListenable: _showLanguageCard,
+          builder: (context, show, _) {
+            return show ? const LanguageListWidget() : const SizedBox.shrink();
+          },
+        )
+      ],
+    );
+  }
+}
 
-    return Consumer(builder: (context, ref, _) {
-      final AppLanguage languageModeState =
-          ref.watch(appLanguageStateProvider); // 전역 state관리
-      final lanuguageNotifier = ref.read(appLanguageStateProvider.notifier);
+class LanguageSwitchWidget extends StatelessWidget {
+  final VoidCallback onTab;
 
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SettigInfoListTileWidget(
-            leadingIcon: Icons.language,
-            trailingWidget: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
-                  color: Theme.of(context).primaryColor),
-              child: Text(
-                languageModeState.langCode,
-                style: TextStyle(
-                  color: Theme.of(context).listTileTheme.selectedColor,
-                ),
+  const LanguageSwitchWidget({super.key, required this.onTab});
+
+  @override
+  Widget build(BuildContext context) {
+    return SettigInfoListTileWidget(
+      leadingIcon: Icons.language,
+      title: 'setting_page.list.language',
+      onTap: onTab,
+      trailingWidget: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Theme.of(context).primaryColor),
+        child: Consumer(
+          builder: (context, ref, _) {
+            final AppLanguage languageModeState =
+                ref.watch(appLanguageStateProvider); // 전역 state관리
+
+            return Text(
+              languageModeState.langCode,
+              style: TextStyle(
+                color: Theme.of(context).listTileTheme.selectedColor,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class LanguageListWidget extends StatelessWidget {
+  const LanguageListWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(
+      builder: (context, ref, _) {
+        final AppLanguage languageModeState =
+            ref.watch(appLanguageStateProvider); // 전역 state관리
+        final lanuguageNotifier = ref.read(appLanguageStateProvider.notifier);
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: AppLanguage.values
+                    .map(
+                      (item) => ListTile(
+                        leading: Text(item.countryCode),
+                        title: Text(item.langCode),
+                        trailing: item.langCode == languageModeState.langCode
+                            ? Icon(
+                                Icons.check,
+                                color: Theme.of(context).primaryColor,
+                              )
+                            : null,
+                        onTap: () {
+                          lanuguageNotifier.setLanguageMode(
+                              context, item.langCode);
+                        },
+                      ),
+                    )
+                    .toList(),
               ),
             ),
-            title: 'setting_page.list.language'.tr(),
-            onTap: () => showLanguageCard.value = !showLanguageCard.value,
           ),
-          if (showLanguageCard.value)
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: AppLanguage.values
-                        .map(
-                          (item) => ListTile(
-                            leading: Text(item.countryCode),
-                            title: Text(item.langCode),
-                            trailing:
-                                item.langCode == languageModeState.langCode
-                                    ? Icon(
-                                        Icons.check,
-                                        color: Theme.of(context).primaryColor,
-                                      )
-                                    : null,
-                            onTap: () {
-                              lanuguageNotifier.setLanguageMode(
-                                  context, item.langCode);
-                            },
-                          ),
-                        )
-                        .toList(),
-                  ),
-                ),
-              ),
-            ),
-        ],
-      );
-    });
+        );
+      },
+    );
   }
 }
